276c8dfd126d779ae77612574fb54c422647fe46
xiamincoon <mincoon@126.com>
2022-05-11 16:05:31 +0800

    Improve api cache support

--- broker/api/api.go
+++ broker/api/api.go
@@ -30,71 +30,84 @@ func Run(stack *hub.Stack) (interface{}, int) {
 
 	var jsonInRspBody interface{}
 	var jsonOutRspBody interface{}
-	var jsonInRspCacheBody interface{}
 
-	apiDef.Token.Lock.RLock()
-	TokenExp := TokenExpired(apiDef)
-	apiDef.Token.Lock.RUnlock()
+	if apiDef.Cache != nil { //如果Json文件中配置了cache，表示支持缓存
+		if CacheExpired(apiDef, true) {
+			klog.Infoln("Cache缓存过期或还未获取缓存，获取Cache")
 
-	if !TokenExp {
-		klog.Infoln("Token缓存过期或还未获取缓存，获取Token")
-		defer apiDef.Token.Lock.Unlock()
+			defer apiDef.Cache.Locker.Unlock()
+			apiDef.Cache.Locker.Lock()
 
-		apiDef.Token.Lock.Lock()
-		if !TokenExpired(apiDef) {
-			outReq := NewRequest(stack, apiDef)
+			if CacheExpired(apiDef, false) {
+				outReq := NewRequest(stack, apiDef)
+				// 发出请求
+				client := &http.Client{}
+				resp, err := client.Do(outReq)
+				if err != nil {
+					klog.Errorln("err", err)
+					return nil, 500
+				}
+				defer resp.Body.Close()
+				returnBody, _ := io.ReadAll(resp.Body)
 
-			// 发出请求
-			client := &http.Client{}
-			resp, err := client.Do(outReq)
-			if err != nil {
-				klog.Errorln("err", err)
-				return nil, 500
-			}
+				// 将收到的结果转为JSON对象
+				json.Unmarshal(returnBody, &jsonInRspBody)
 
-			defer resp.Body.Close()
-			returnBody, _ := io.ReadAll(resp.Body)
-
-			// 将收到的结果转为JSON对象
-			json.Unmarshal(returnBody, &jsonInRspBody)
-
-			//解析过期时间，如果存在则记录下来
-			//str := `{"msg":"鉴权成功","expireTime":"600","ak":"MTY1MjEMTAwMU1UWTFNakUyT0RFeU1UUTNNeU14TURBd01USTJNQT09","resultcode":"1"}`
-			//str := `{"msg":"鉴权成功","expireTime":"20220510153521","ak":"MTY1MjEMTAwMU1UWTFNakUyT0RFeU1UUTNNeU14TURBd01USTJNQT09","resultcode":"1"}`
-			//expires, ok := HandleExpires(stack, resp, str, apiDef)
-			expires, ok := HandleExpires(stack, resp, string(returnBody), apiDef)
-			if !ok {
-				klog.Warningln("没有查询到过期时间")
-				jsonInRspCacheBody = jsonInRspBody
-				apiDef.Token.Cache = false
+				//解析过期时间，如果存在则记录下来
+				//str := `{"msg":"鉴权成功","expireTime":"20220510153521","ak":"MTY1MjEMTAwMU1UWTFNakUyT0RFeU1UUTNNeU14TURBd01USTJNQT09","resultcode":"1"}`
+				//expires, ok := HandleExpireTime(stack, resp, str, apiDef)
+				expires, ok := HandleExpireTime(stack, resp, string(returnBody), apiDef)
+				if !ok {
+					klog.Warningln("没有查询到过期时间")
+					// 构造发送的响应内容
+					jsonOutRspBody = NewOutRspBody(apiDef, jsonInRspBody)
+				} else {
+					klog.Infof("更新Cache信息，过期时间为: %v", expires)
+					apiDef.Cache.Expires = expires
+					jsonOutRspBody = NewOutRspBody(apiDef, jsonInRspBody)
+					apiDef.Cache.Resp = jsonOutRspBody
+				}
 			} else {
-				klog.Infof("更新Token信息，过期时间为: %v", expires)
-				apiDef.Token.Cache = true
-				apiDef.Token.Expires = expires
-				apiDef.Token.Resp = jsonInRspBody
-				jsonInRspCacheBody = apiDef.Token.Resp
+				klog.Infoln("Cache缓存有效，直接回应")
+				jsonOutRspBody = apiDef.Cache.Resp
 			}
 		} else {
-			klog.Infoln("Token缓存有效，直接回应")
-			jsonInRspCacheBody = apiDef.Token.Resp
+			klog.Infoln("Cache缓存有效，直接回应")
+			apiDef.Cache.Locker.RLock()
+			jsonOutRspBody = apiDef.Cache.Resp
+			apiDef.Cache.Locker.RUnlock()
 		}
-	} else {
-		klog.Infoln("Token缓存有效，直接回应")
-		apiDef.Token.Lock.RLock()
-		jsonInRspCacheBody = apiDef.Token.Resp
-		apiDef.Token.Lock.RUnlock()
-	}
+	} else { //不支持缓存，直接请求
+		outReq := NewRequest(stack, apiDef)
+		// 发出请求
+		client := &http.Client{}
+		resp, err := client.Do(outReq)
+		if err != nil {
+			klog.Errorln("err", err)
+			return nil, 500
+		}
+		defer resp.Body.Close()
+		returnBody, _ := io.ReadAll(resp.Body)
+		// 将收到的结果转为JSON对象
+		json.Unmarshal(returnBody, &jsonInRspBody)
 
-	// 构造发送的响应内容
-	if apiDef.Response != nil && apiDef.Response.Json != nil {
-		jsonOutRspBody = util.Json2Json(jsonInRspCacheBody, apiDef.Response.Json)
-	} else {
-		// 直接转发返回的结果
-		jsonOutRspBody = jsonInRspCacheBody
+		jsonOutRspBody = NewOutRspBody(apiDef, jsonInRspBody)
 	}
 
 	klog.Infoln("处理", apiDef.Url, ":", http.StatusOK, "\r\n返回结果：", jsonOutRspBody)
 	return jsonOutRspBody, http.StatusOK
+}
+
+// 构造发送的响应内容
+func NewOutRspBody(apiDef *hub.ApiDef, in interface{}) interface{} {
+	var out interface{}
+	if apiDef.Response != nil && apiDef.Response.Json != nil {
+		out = util.Json2Json(in, apiDef.Response.Json)
+	} else {
+		// 直接转发返回的结果
+		out = in
+	}
+	return out
 }
 
 func NewRequest(stack *hub.Stack, apiDef *hub.ApiDef) *http.Request {
@@ -125,7 +138,6 @@ func NewRequest(stack *hub.Stack, apiDef *hub.ApiDef) *http.Request {
 
 	// 发出请求的URL
 	outReqURL, _ := url.Parse(apiDef.Url)
-
 	// 设置请求参数
 	outReqParamRules := apiDef.Parameters
 	if outReqParamRules != nil {
@@ -193,35 +205,35 @@ func NewRequest(stack *hub.Stack, apiDef *hub.ApiDef) *http.Request {
 	return outReq
 }
 
-func HandleExpires(stack *hub.Stack, resp *http.Response, body string, apiDef *hub.ApiDef) (time.Time, bool) {
-	//首先在api 的json文件中配置参数
-	//expire_key：from 为从header还是从body中获取过期时间
-	//            name 为获取过期时间的关键字串
-	//expire_unit：from 目前有两种类型，date格式和second数
-	//            name 如果是date格式，则配置具体格式串，如果是second数，则name会忽略
-	var src, key, unit, format string
-	outReqParamRules := apiDef.Parameters
-	if outReqParamRules != nil {
-		paramLen := len(*outReqParamRules)
-		if paramLen > 0 {
-			for _, param := range *outReqParamRules {
-				if len(param.Name) > 0 {
-					if param.Name == "expire_key" {
-						src = param.From.From
-						key = param.From.Name
-					} else if param.Name == "expire_unit" {
-						unit = param.From.From
-						format = param.From.Name
-					}
-				}
-			}
-		}
+func HandleExpireTime(stack *hub.Stack, resp *http.Response, body string, apiDef *hub.ApiDef) (time.Time, bool) {
+	//首先在api 的json文件中配置参数 cache
+	// "cache": {
+	// 	"from": {
+	// 		"from": "header",
+	// 		"name": "Set-Cookie.expires"
+	// 	},
+	// 	"format": "Mon, 02-Jan-06 15:04:05 MST"
+	//   }
+	//from 为从header还是从body中获取过期时间
+	//name 为获取过期时间的关键字串
+	//format：如果是date格式，则配置具体格式串，如果是second数，则按照秒数解析
+	//	baidu_image_classify_token: Mon, 02-Jan-06 15:04:05 MST
+	//	body中一个例子："expireTime":"20220510153521",格式为：20060102150405
+
+	var src, key, format string
+	src = apiDef.Cache.From.From
+	key = apiDef.Cache.From.Name
+	format = apiDef.Cache.Format
+	klog.Infoln("获得参数，[src]:", src, "; [key]:", key, "; [format]:", format)
+
+	if src == "" || key == "" || format == "" {
+		klog.Warningln("Json文件中未配置过期时间参数")
+		return time.Time{}, false
 	}
 
-	klog.Infoln("获得参数，[src]:", src, "; [key]:", key, "; [unit]:", unit, "; [format]:", format)
-
-	if src == "header" {
-		if key == "expires" {
+	if strings.EqualFold(src, "header") {
+		if strings.Contains(key, "Set-Cookie.") {
+			key = strings.TrimPrefix(key, "Set-Cookie.")
 			//判断Set-Cookie中是否含有Expires 的header
 			cookie := resp.Header.Get("Set-Cookie")
 			klog.Infoln("Header中Set-Cookie: ", cookie)
@@ -229,10 +241,9 @@ func HandleExpires(stack *hub.Stack, resp *http.Response, body string, apiDef *h
 				expiresIndex := strings.Index(cookie, key) //"expires="
 				if expiresIndex >= 0 {
 					semicolonIndex := strings.Index(cookie[expiresIndex:], ";")
-					expiresStr := cookie[expiresIndex+8 : expiresIndex+semicolonIndex]
-					expires, err := ParseExpires(expiresStr, unit, format)
+					expiresStr := cookie[expiresIndex+len(key)+1 : expiresIndex+semicolonIndex]
+					expires, err := ParseExpireTime(expiresStr, format)
 					if err == nil {
-						klog.Infoln("消息头中Set-Cookie的过期时间: ", expires)
 						// klog.Infoln("Mock now: ", time.Now())
 						// klog.Infoln("Mock Expires: ", time.Now().Add(60000000000))
 						// return time.Now().Add(60000000000), true
@@ -240,32 +251,31 @@ func HandleExpires(stack *hub.Stack, resp *http.Response, body string, apiDef *h
 					}
 				}
 			}
-		} else if key == "Expires" {
+		} else {
 			//判断是否含有Expires 的header
 			expireHeader := resp.Header.Get(key)
 			klog.Infoln("Header中Expires: ", expireHeader)
 			if len(expireHeader) > 0 {
-				expires, err := ParseExpires(expireHeader, unit, format)
+				expires, err := ParseExpireTime(expireHeader, format)
 				if err == nil {
-					klog.Infoln("消息头中Expires的过期时间: ", expires)
 					return expires, true
 				}
 			}
 		}
-
-	} else if src == "body" {
+	} else if strings.EqualFold(src, "body") {
+		//例"expireTime":"20220510153521",
 		index := strings.Index(body, key)
 		if index >= 0 {
 			comma := strings.Index(body[index:], ",")
 			if comma >= 0 {
-				timestr := body[index+12 : index+comma]
-
+				timestr := body[index+len(key)+2 : index+comma]
+				timestr = strings.TrimSpace(timestr)
 				timestr = strings.TrimPrefix(timestr, `"`)
 				timestr = strings.TrimSuffix(timestr, `"`)
 				klog.Infoln("消息体中过期时间:", timestr)
-				formatTime, err := ParseExpires(timestr, unit, format)
+
+				formatTime, err := ParseExpireTime(timestr, format)
 				if err == nil {
-					klog.Infoln("消息体解析后过期时间:", formatTime)
 					return formatTime, true
 				}
 			}
@@ -277,35 +287,37 @@ func HandleExpires(stack *hub.Stack, resp *http.Response, body string, apiDef *h
 	return time.Time{}, false
 }
 
-func ParseExpires(str string, unit string, format string) (time.Time, error) {
+func ParseExpireTime(str string, format string) (time.Time, error) {
 	var exptime time.Time
 	var err error
-	if unit == "second" {
+
+	if format == "second" {
 		var s int
 		s, err = strconv.Atoi(str)
 		if err != nil {
-			klog.Errorln("ParseExpires秒数失败")
+			klog.Errorln("解析过期时间失败, err: ", err)
 			return time.Time{}, errors.New("Parse expires failed")
 		}
 
 		exptime = time.Now()
 		exptime = exptime.Add(time.Second * time.Duration(s))
-		klog.Infoln("按照second解析Expires: ", exptime)
-	} else if unit == "date" {
+	} else {
 		exptime, err = time.Parse(format, str)
 		if err != nil {
-			klog.Infoln("解析Expires失败:")
+			klog.Errorln("解析过期时间失败, err: ", err)
 			return time.Time{}, errors.New("Parse expires failed")
 		}
-		klog.Infoln("按照date解析Expires: ", exptime)
 	}
-
+	klog.Infoln("解析后过期时间: ", exptime)
 	return exptime.Local(), nil
 }
 
-func TokenExpired(apiDef *hub.ApiDef) bool {
-	//判断是否支持缓存，如果支持，并且没有过期，则直接返回结果，
-	//如果不支持缓存，则请求，并且判断过期时间，并修改缓存标志
-	//如果失效了（过期了），则加锁然后获取新的过期时间
-	return apiDef.Token.Cache && time.Now().Local().Before(apiDef.Token.Expires)
+func CacheExpired(apiDef *hub.ApiDef, lock bool) bool {
+	//如果支持缓存，判断过期时间
+	if lock {
+		apiDef.Cache.Locker.RLock()
+		defer apiDef.Cache.Locker.RUnlock()
+		return time.Now().Local().After(apiDef.Cache.Expires)
+	}
+	return time.Now().Local().After(apiDef.Cache.Expires)
 }
