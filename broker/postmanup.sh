cc953aa0925bfca64377cfc673c7d873a4c569d2
shengzmhome <1300163238@qq.com>
2022-08-07 21:20:07 +0800

    增加httpserver程序检查、编译、启动过程

--- broker/postmanup.sh
+++ broker/postmanup.sh
@@ -2,28 +2,25 @@
 # ############################################################
 # 可配置文件路径及名称：
 # 
-# postman_collection_addr：     postman_collection文件相对位置
-# postman_environment_addr：    postman_environment文件相对位置
+# postman_collection_app：     postman_collection文件相对位置
+# postman_environment_app：    postman_environment文件相对位置
 # 
 # ############################################################
 
-postman_collection_addr="./APIHUB_0623.postman_collection"
-postman_environment_addr="./34test_0623.postman_environment"
+postman_collection_app="./APIHUB_0623.postman_collection"
+postman_environment_app="./34test_0623.postman_environment"
 
 # ############################################################
 
 
-echo "-------auto run postman-------"
-
-run1="./APIHUB_0623.postman_collection"
-run2="./34test_0623.postman_environment"
-if [ -f $run1 -a $run2 ];then
-    newman run $postman_collection_addr -e $postman_environment_addr
+echo "正在启动postman测试程序"
+echo "--------------auto run postman--------------"
+if [ -f $postman_collection_app -a $postman_environment_app ];then
+    newman run $postman_collection_app -e $postman_environment_app
     echo "success: postman运行结束"
 else
     echo "error: ./*.postman_collection 和 *.postman_environment 文件不存在"
 fi
 
-# newman run APIHUB_0623.postman_collection -e 34test_0623.postman_environment
 
-# echo "success: "postman测试启动成功"
+
