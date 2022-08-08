255e7a8e3de2c1a884d2c42a0453ab1222aac9e4
Sheng-ZM <shengzhenming@live.com>
2022-08-08 09:39:11 +0800

    修复httpserver启动bug

--- broker/startup-postmanup.sh
+++ broker/startup-postmanup.sh
@@ -21,6 +21,7 @@ postman_collection_app="./APIHUB_0623.postman_collection"
 postman_environment_app="./34test_0623.postman_environment"
 
 httpserver_app="../test/http-server/http-server"
+httpserver_path="../test/http-server/"
 httpserver_ip="127.0.0.1:6060"
 
 ##############################################################
@@ -52,12 +53,15 @@ if [ -f "$httpserver_app" ];then
     $httpserver_app --addr $httpserver_ip &
     echo "success: http-server后台运行成功!"
 else
-    echo "error: .../test/http-server/http-server 可执行文件不存在"
+    echo "error: ../test/http-server/http-server 可执行文件不存在"
+    cd $httpserver_path
+    echo "running: 正在重新编译..."
+    go get
     go build -o http-server
+    cd -
     if [ -f "$httpserver_app" ];then
         $httpserver_app --addr $httpserver_ip &
         echo "success: http-server重新编译且后台运行成功!"
-        go get
     else
         echo "error: http-server重新编译源码失败, 请检查 .../test/http-server/ 源码是否正确"
         echo "http-server应用程序启动失败, 未找到源码或可执行文件, 请检查shell文件路径"
@@ -71,11 +75,12 @@ if [ -f "$apihub_app" ];then
     echo "success: tms-go-apihub后台运行成功!"
 else
     echo "error: ./tms-go-apihub 可执行文件不存在"
+    echo "running: 正在重新编译..."
+    go get
     go build -o tms-go-apihub
     if [ -f "$apihub_app" ];then
         $apihub_app --base $conf_path &
         echo "success: tms-go-apihub重新编译且后台运行成功!"
-        go get
     else
         echo "error: tms-go-apihub重新编译源码失败, 请检查 ./ 源码是否正确"
         echo "tms-go-apihub应用程序启动失败, 未找到源码或可执行文件, 请检查shell文件路径"
