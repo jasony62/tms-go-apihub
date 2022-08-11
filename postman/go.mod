0bf29e4ad6bd59220a431cbac1c29525d98adf2a
Sheng-ZM <shengzhenming@live.com>
2022-08-11 11:47:46 +0800

    0811暂存

--- postman/go.mod
+++ postman/go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/goccy/go-yaml v1.9.5 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/rbretecher/go-postman-collection v0.8.0 // indirect
 	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- postman/main.go
+++ postman/main.go
@@ -4,8 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+
+	"os"
 	"strings"
 
+	"github.com/rbretecher/go-postman-collection"
 	"k8s.io/klog/v2"
 )
 
@@ -29,21 +32,25 @@ type Args struct {
 	Value Value  `json:"value"`
 }
 
+// postman文件路径
 var postmanPath string
 
-// var postmanCollectionBytes postman
+// json结构体
+var apiHubHttpConf ApiHubHttpConf
 
+// 初始化
 func init() {
 	flag.StringVar(&postmanPath, "from", "./postman_collections/", "指定postman_collections文件路径")
 	// flag.StringVar(&apiHubConfPath, "to", "./to/", "指定转换后的apiHubConf json文件路径")
 }
 
+/*********************main主程序*****************************/
 func main() {
 	convertPostmanFiles(postmanPath)
 }
 
+// Postman文件转换函数
 func convertPostmanFiles(path string) {
-
 	// 读取指定目录下文件信息list
 	fileInfoList, err := ioutil.ReadDir(path)
 	if err != nil {
@@ -54,39 +61,78 @@ func convertPostmanFiles(path string) {
 	var prefix string
 	// 遍历postman_collection文件
 	for i := range fileInfoList {
-
 		fileName := fmt.Sprintf("%s/%s", path, fileInfoList[i].Name())
-		// 是否是一个子目录
+		// 是否是一个子目录 若是子目录，进入进入子目录遍历postman文件
 		if fileInfoList[i].IsDir() {
-			klog.Infoln("postman_collections子目录fileName: ", fileName)
+			klog.Infoln("__postman_collections子目录名: ", fileName)
 			prefix = fileInfoList[i].Name()
 			convertPostmanFiles(path + "/" + prefix)
 		} else {
 			// 判断文件是否postman_collection类型
-			if !strings.HasSuffix(fileName, ".postman_collection") && !strings.HasSuffix(fileName, ".postman_collection.json") {
+			if !strings.HasSuffix(fileName, ".postman_collection") && !strings.HasSuffix(fileName, ".json") {
 				continue
 			}
-			klog.Infoln("######加载postman_collection(*.postman_collection or .postman_collection.json)文件: ", fileName)
-			// ReadFil读取文件内容到fileBytes
-			// fileBytes, err := ioutil.ReadFile(fileName)
-			// if err != nil {
-			// 	klog.Errorln(err)
-			// 	panic(err)
-			// }
-			// covertPostmanToApihubConf(fileInfoList[i])
+			klog.Infoln("__Load postman_collection文件: ", fileName)
+
+			// 读取文件内容到fileBytes
+			file, err := os.Open(fileName)
+			if err != nil {
+				klog.Errorln(err)
+				panic(err)
+			}
+			defer file.Close()
+
+			// Parse the contents
+			postmanfileBytes, err := postman.ParseCollection(file)
+			if err != nil {
+				klog.Errorln(err)
+				panic(err)
+			}
+			// _ = postmanfileBytes
+
+			for j := range postmanfileBytes.Items {
+				covertOneRequest(postmanfileBytes.Items[j])
+			}
 		}
 	}
 }
 
-// func covertPostmanToApihubConf(fileInfoList []fs.FileInfo) {
-// 	var err error
-// 	postmanCollectionBytes, err = postman.ParseCollection(fileInfoList)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
+func covertOneRequest(postmanItem *postman.Items) {
 
-// 	// getServerUrl(postmanCollectionBytes)
-// 	// parsePaths(postmanCollectionBytes)
-// 	// parseRequestBodies(postmanCollectionBytes.Components.RequestBodies) //?
-// 	// generateApiHubConf(postmanCollectionBytes)
-// }
+	// 	// _ = postmanfileBytes
+	getHttpapiInfo(postmanItem)
+	getHttpapiArgs(postmanItem.Request.URL)
+}
+
+func getHttpapiInfo(postmanItem *postman.Items) {
+
+	apiHubHttpConf.ID = postmanItem.Name
+	klog.Infoln("__request Name : ", apiHubHttpConf.Description)
+
+	apiHubHttpConf.Description = postmanItem.Name
+	klog.Infoln("__request Description : ", apiHubHttpConf.Description)
+
+	apiHubHttpConf.URL = postmanItem.Request.URL.Raw
+	// getdel := fmt.Sprintf("?")
+	// for i := range postmanItem.Responses.URL.Path {
+	// getdel := strings.Join(postmanItem.Request.URL.Path)
+	// }
+	// apiHubHttpConf.URL = strings.TrimSuffix(apiHubHttpConf.URL, getdel)
+	klog.Infoln("__request URL : ", apiHubHttpConf.URL)
+
+	apiHubHttpConf.Method = string(postmanItem.Request.Method)
+	klog.Infoln("__request Method : ", apiHubHttpConf.Method)
+
+	// parseEvent(postmanItem)
+	// apiHubHttpConf.Requestcontenttype = "none" // default content
+	// apiHubHttpConf.Private = "none" // default private content
+}
+
+func getHttpapiArgs(postmanURL *postman.URL) {
+	// for i := range list(postmanURL.Query) {
+	if postmanURL.Query == "query" {
+		args := Args{In: "query", Name: "", Value: Value{From: "query", Content: ""}}
+		apiHubHttpConf.Args = append(apiHubHttpConf.Args, args)
+	}
+	// }
+}
